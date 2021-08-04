c59aa87dcf26f28abf041303385953baeb757451
William Rotch <williamrotchIB@gmail.com>
2021-08-04 11:05:01 -0400

    Set up database Connection

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-const express = require('express')
-require("dotenv").config
-const app = express()
+const express = require("express");
+require("dotenv").config;
+const app = express();
 //setting up default port
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 //Binding our server to a static directory
-app.use(express.static("./public"))
-
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
 //setting up mongo conncetion
-const mongoose = require("mongoose")
-const databaseAuthorization = process.env.SECRET
+const mongoose = require("mongoose");
+const databaseAuthorization = process.env.SECRET;
+console.log(databaseAuthorization)
 //set up path for connection, using .env for the password
-const uri = `mongodb+srv://binderApp:${databaseAuthorization}@test.ws3nz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const uri = `mongodb+srv://binderApp:binderapp123@test.ws3nz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 //connect to the db
-mongoose.conncet(uri, {
-    useNewURLParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(uri, {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+});
 //var to refer to the database
-const db = mongoose.connection
+const db = mongoose.connection;
+//set up a schema to test
+const testSchema = new mongoose.Schema({
+  email: String,
+});
+const emailTest = mongoose.model("Email-Test", testSchema);
+
+app.post("/", async (req, res) => {
+    console.log(`Hel2l5552oz`)
+    console.log(req.body.email)
+   let newEntry = emailTest({
+        email: req.body.email
+    })
+await newEntry.save()
+res.redirect('/')
+}) 
 
 
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
-})
+  console.log(`Listening on port: ${port}`);
+});
