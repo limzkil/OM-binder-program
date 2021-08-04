a84ef1228d0a79875ad358986020b9f5080c31db
manofshad97 <shadman559@hotmail.com>
2021-08-04 10:57:14 -0400

    Created email automation for regular form

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
-const express = require('express')
-
+const express = require("express")
+const cors = require("cors")
 const app = express()
-//setting up default port
-const port = process.env.PORT || 5000
-//Binding our server to a static directory
+const nodemailer = require("nodemailer")
+require("dotenv").config()
+
 app.use(express.static("./public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
+app.post("/send_mail", async (req, res) => {
+	let { email, number, address } = req.body
+	const transport = nodemailer.createTransport({
+		service: "Gmail",
+		auth: {
+			user: process.env.GMAIL_USER,
+			pass: process.env.GMAIL_PASS
+		}
+	})
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
+	await transport.sendMail({
+		from: process.env.GMAIL_USER,
+		to: email,
+		subject: "test email",
+		html: `<div className="email" style="
+        border: 1px solid black;
+        padding: 20px;
+        font-family: sans-serif;
+        line-height: 2;
+        font-size: 20px; 
+        ">
+        <h2>Please verify that the information below is correct!</h2>
+        <p><strong>Email:</strong> ${email}</p>
+		<p><strong>Phone number:</strong> ${number}</p>
+		<p><strong>Address:</strong> ${address}</p>
+    
+        <p>All the best, Shadman</p>
+         </div>
+    `
+	})
 })
+
+app.listen(
+	5000,
+	() => {
+		console.log("Server is listening on port 5000")
+	})
--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import ReqForm from './ReqForm'
+import React, { useState } from "react"
+// import "./App.css"
 
-export default function App() {
+function App() {
+  const [sent, setSent] = useState(false)
+  const [email, setEmail] = useState("")
+  const [number, setNumber] = useState("")
+  const [address, setAddress] = useState("")
+  const handleSend = async (e) => {
+    setSent(true)
+    try {
+      await fetch("http://localhost:5000/send_mail", {
+        body: JSON.stringify({
+          email: email,
+          number: number,
+          address: address
+        }),
+        headers: {"content-type": "application/json"},
+        method: "POST"
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
-    <>
-    <ReqForm />
-    <div>HellooOO</div>
-    </>
-  )
+    <div className="App">
+      {!sent ? (
+        // <form onSubmit={handleSend}>
+        <div>
+          <input type="text" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="text" placeholder="Enter your number" value={number} onChange={(e) => setNumber(e.target.value)} />
+          <input type="text" placeholder="Enter your address" value={address} onChange={(e) => setAddress(e.target.value)} />
 
+          <button onClick={handleSend}>Submit Form</button>
+        </div>
+
+        // </form>
+      ) : (
+        <h1>A confirmation email has been sent to the address provided.</h1>
+      )}
+    </div>
+  )
 }
+
+export default App
