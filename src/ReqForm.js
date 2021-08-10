1a725cc68acbd5cf923838843de27d8403a48810
limzkil <78449568+limzkil@users.noreply.github.com>
2021-08-10 10:15:27 -0400

    Update ReqForm.js

--- src/ReqForm.js
+++ src/ReqForm.js
@@ -1,54 +1,11 @@
-import { useFormik } from "formik";
+
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Select from "@material-ui/core/Select"
+
 import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import axios from "axios"
+
 
 export default function ReqForm() {
-<<<<<<< Updated upstream
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      resMaine: false
-    },
-
-    onSubmit: (values) => {
-      console.log(values)
-      axios.post('http://localhost:3000/')
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
-  return (
-    <>
-      <form action='/' method='POST'>
-        <InputLabel>Are you a resident of Maine?</InputLabel>
-        <Select 
-        id = "resMaine"
-        name = "resMaine"
-        value = {formik.values.resMaine}
-        onChange = {formik.handleChange} >
-
-          <MenuItem value = {true}>Yes</MenuItem>
-          <MenuItem value = {false}>No</MenuItem>
-
-        </Select>
-
-        <TextField
-          
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
-        <Button color="primary" variant="contained"  type="submit">
-          Submit
-        </Button>
-      </form>
-=======
   const [sent, setSent] = useState(false);
   const [nameSelf, setNameSelf] = useState("");
   const [emailSelf, setEmailSelf] = useState("");
@@ -359,7 +316,6 @@ export default function ReqForm() {
           <h1>A confirmation email has been sent to the address provided.</h1>
         )}
       </Container>
->>>>>>> Stashed changes
     </>
   );
 }
