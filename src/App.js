98e69e687ea302dbe5df9a9b69384f0c2a9b38ea
Allan Hall <flashfantasy@outlook.com>
2021-08-11 17:10:36 -0400

    syntax edits

--- src/App.js
+++ src/App.js
@@ -1,79 +1,62 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid, GridExportCsvOptions } from "@material-ui/data-grid";
 
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import logo from './logo.svg';
-import './App.css';
-const axios = require('axios').default;
+import logo from "./logo.svg";
+import "./App.css";
+const axios = require("axios").default;
 
 export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-const [isLoaded,setIsLoaded] = useState(false);
+  const [rowData, setRowData] = useState([]);
 
-const [rowData,setRowData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/binders") //
 
-useEffect(() => {
-
-
-axios
-
-.get('http://localhost:9000/binders') // 
-
-.then((response) => response.data)
+      .then((response) => response.data)
       .then((data) => {
         setIsLoaded(true);
         setRowData(data);
       });
   }, []);
 
-// MATERIAL DATAGRID
 
-const columns = [
+  // MATERIAL DATAGRID
 
-{ field: '_id', headerName: '_id', width: 250, editable: true  },
+  const columns = [
+    { field: "_id", headerName: "_id", width: 250, editable: true, },
 
-{ field: 'id', headerName: 'ID', width: 100, editable: true  },
+    { field: "size", headerName: "Size", width: 250, editable: true },
 
-{ field: 'size', headerName: 'Size', width: 250, editable: true },
+    { field: "color", headerName: "Color", width: 250, editable: true },
 
-{ field: 'color', headerName: 'Color', width: 250, editable: true  },
+    { field: "length", headerName: "Length", width: 250, editable: true },
 
-{ field: 'length', headerName: 'Length', width: 250, editable: true  },
+    {
+      field: "dateSaved",
+      headerName: "Date Added",
+      width: 250,
+      editable: true,
+    },
+  ];
 
-{ field: 'dateSaved', headerName: 'Date Added', width: 250, editable: true  },
-
-];
-
-return(
-
-<div style={{ height: 700, width: '100%' }}>
-
-<div style={{ display: 'flex', height: '100%' }}>
-
-<div style={{ flexGrow: 1 }}>
-
-<DataGrid
-
-columns={columns}
-
-rows={rowData}
-
-getRowId ={(row) => row._id}
-
-id='_id'
-
-/>
-
-</div>
-
-</div>
-
-</div>
-
-);
-
+  return (
+    <div style={{ height: 700, width: "100%" }}>
+      <div style={{ display: "flex", height: "100%" }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid
+            columns={columns}
+            rows={rowData}
+            getRowId={(row) => row._id}
+            id="_id"
+          />
+        </div>
+      </div>
+    </div>
+    
+  );
 }
-
-
