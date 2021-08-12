8d74ce386e109bf156dd30ba53bfe016b0ac363b
William Rotch <williamrotchIB@gmail.com>
2021-08-12 10:10:48 -0400

    Adding some CRUD ICons

--- src/fetchComponents/Inventory.js
+++ src/fetchComponents/Inventory.js
@@ -1,15 +1,16 @@
-
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import TableRowColumn from "@material-ui/core/Table";
 import Typography from "@material-ui/core/Typography";
-
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Inventory(props) {
-    console.log(props)
+  
   return (
     <TableContainer className="inventoryContainer">
       <Table className="inventoryEntry">
@@ -19,8 +20,10 @@ function Inventory(props) {
             <TableCell>Size</TableCell>
             <TableCell>Length</TableCell>
             <TableCell>Color</TableCell>
+           
           </TableRow>
         </TableHead>
+       
         {props.inventoryData.map((inventory, index) => (
           <>
             <TableBody key={index}>
@@ -36,6 +39,12 @@ function Inventory(props) {
               <TableCell>
                 <Typography variant="subtitle1">{inventory.color}</Typography>
               </TableCell>
+              <TableRowColumn>
+              <EditIcon/>
+            </TableRowColumn>
+            <TableRowColumn>
+              <DeleteIcon />
+            </TableRowColumn>
             </TableBody>
           </>
         ))}
