8a6cb35ab0e299a33506a4402b0eb6c111033e21
manofshad97 <shadman559@hotmail.com>
2021-08-18 12:34:51 -0400

    fixed merge error

--- Schema.js
+++ Schema.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 
 const CreateSchema = new mongoose.Schema({
   county: String,
@@ -20,25 +19,3 @@ const CreateSchema = new mongoose.Schema({
 })
 
 module.exports = CreateSchema
-=======
- 
-        const CreateSchema = new mongoose.Schema({
-          county: String,
-          elseEmail: String,
-          elsePhone: Number,
-          nameSelf: String,
-          nameElse: String,
-          dob: String,
-          email: String,
-          phone: Number,
-          address: String,
-          size: String,
-          length: String,
-          color: String,
-          willWait: Boolean,
-          moreInfo: String,
-          date: Date,
-        })
-
-        module.exports = CreateSchema
->>>>>>> 6ec96532da50a1a986b2f11b601e411239535b0f
